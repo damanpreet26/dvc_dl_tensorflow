42971b98f01cc537182409d61c7770d925738a70
damanpreet26 <damanpreets26@gmail.com>
2021-12-25 22:46:48 +0530

    setup.py file added

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("requirements.txt","r+") as f:
+    desc = f.read()
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="demon",
+    description="A small package for dvc pipeline",
+    long_description=long_desc,
+    url="https://github.com/damanpreet26/dvc_dl_tensorflow",
+    author_email="damanpreets26@gmail.com",
+    package=["src"],
+    license="GPU",
+    python_requires=">=3.7",
+    install_requires=[
+        dvc,
+        tensorflow,
+        numpy,
+        matplotlib,
+        pandas,
+        tqdm,
+        PyYAML,
+        boto3
+    ]
+)
